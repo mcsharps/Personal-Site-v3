26cda23d6026c15ed146ba681ad586f91c24e800
Madison C Sharps <madison.sharps@iscs.com>
2016-03-07 21:12:21 -0600

    wow db stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,8 +12,26 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+ // response.render('pages/index');
+  var result = '';
+  var times = process.env.TIMES || 5;
+  for (var i=0; i < times; i++)
+	result += cool();
+  response.send(result);
+
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.get('/cool', function(request, response){
 response.send(cool());
