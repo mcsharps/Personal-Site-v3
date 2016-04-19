1887afdc6d7564929e9bf625c26462d7a2ed3fda
Madison C Sharps <madison.sharps@iscs.com>
2016-04-18 20:39:56 -0500

    add gulpfile tasks to include webpack and postcss as well as package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,64 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-
-gulp.task('default', function() {
-     gulp.src(['index.js'])
-        .pipe(babel({
-            presets: ['es2015', 'react']
-        }))
-        .pipe(gulp.dest('release'));
-
-    return gulp.src('components/*.js')
-        .pipe(babel({
-            presets: ['es2015', 'react']
-        }))
-        .pipe(gulp.dest('release/components'));
+var webpack = require('webpack');
+var path = require('path');
+var bunyan = require('bunyan');
+var postcss = require('gulp-postcss');
+var cssvariables = require('postcss-css-variables');
+var autoprefixer = require('autoprefixer');
+var lost = require('lost');
+var log = bunyan.createLogger({
+  name: 'twitterAndStrava',
+  streams: [
+    {
+      level: 'info',
+      // stream: process.stdout,
+      path: './myAppInfo.log'            // log INFO and above to stdout
+    },
+    {
+      level: 'error',
+      path: './myAppErrors.log'  // log ERROR and above to a file
+    }
+  ]
 });
+var webpackBabelConfig = {
+  entry: path.join(process.cwd(), 'client-render.js'),
+  output: {
+    path: './public/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+gulp.task('webpack', function() {
+  return webpack(webpackBabelConfig).run(function(err, stats){
+        if (err) {
+            console.log(err);
+            log.info(err);
+        }
+        else {
+            console.log(stats.toString());
+            log.info(stats.toString());
+        }
+
+    });
+});
+gulp.task('postcss', function(){
+    var processers = [
+    autoprefixer({ browsers: ['last 2 versions'] }),
+    cssvariables,
+    lost
+    ];
+    return gulp.src('*.css')
+           .pipe(postcss(processers))
+           .pipe( gulp.dest('public/') );
+});
+
+gulp.task('default', ['webpack', 'postcss']);
