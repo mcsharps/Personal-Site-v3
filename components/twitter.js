02b11d446fcc29aa1ad31665e4575180435f2908
Madison C Sharps <madison.sharps@iscs.com>
2016-05-02 22:14:11 -0500

    start styling biking weather and move twitter header into react component

--- components/twitter.js
+++ components/twitter.js
@@ -4,7 +4,7 @@ export default class TwitterComponent extends React.Component {
   render() {
     return (
       <div>
-        <p>This is the twitter react component</p>
+        <h2>Twitter Results for #feelthebern within 1000 miles of Birmingham</h2>
         <div>
         </div>
       </div>
